0a8a674e467dd800da18901cd4f3b779cbb06d8a
daalegend27 <46014283+daalegend27@users.noreply.github.com>
2019-11-20 23:14:45 -0700

    Add files via upload
    full gui directory

---
+++ cse360project1/src/cse360project1/GUI2.java
@@ -0,0 +1,54 @@
+package cse360project1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.FlowLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+
+public class GUI2 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI2 window = new GUI2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI2() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[]", "[]"));
+		
+		JButton btnNewButton = new JButton("New button");
+		frame.getContentPane().add(btnNewButton, "cell 0 0");
+	}
+
+}
